86e90dfa3f45199b3f5a1cd910f7155dbb9a7f63
nils-hamel <nils.hamel@bluewin.ch>
2020-09-09 12:22:58 +0200

    Optimisation of the strict symetric distance detection algorithm

--- src/eratosthene-operator.c
+++ src/eratosthene-operator.c
@@ -53,7 +53,7 @@
         while ( ( le_pri_head - le_pri_base ) < le_pri_size ) {
 
             /* extract primitive type */
-            le_pri_type = * ( le_pri_head + LE_ARRAY_DATA_POSE );
+            le_pri_type = * ( le_pri_head + LE_ARRAY_DATA_POSE ) * LE_ARRAY_DATA;
 
             /* reset detection flag */
             le_flag = 0x00;
@@ -62,10 +62,10 @@
             le_sec_head = le_sec_base;
 
             /* parsing secondary */
-            while ( ( le_flag == 0x00 ) && ( ( le_sec_head - le_sec_base ) < le_sec_size ) ) {
+            while ( ( ! le_flag ) && ( ( le_sec_head - le_sec_base ) < le_sec_size ) ) {
 
                 /* extract primitive type */
-                le_sec_type = * ( le_sec_head + LE_ARRAY_DATA_POSE );
+                le_sec_type = * ( le_sec_head + LE_ARRAY_DATA_POSE ) * LE_ARRAY_DATA;
 
                 /* check type compatibility */
                 if ( le_pri_type == le_sec_type ) {
@@ -77,7 +77,7 @@
                     le_index = 0;
 
                     /* parsing primitive vertex */
-                    while ( ( le_galf == 0xff ) && ( le_sec_type > 0 ) ) {
+                    while ( ( le_galf ) && ( le_sec_type ) ) {
 
                         /* primitive identity detection procedure */
                         if ( ( ( le_real_t * ) ( le_sec_head + le_index ) )[0] == ( ( le_real_t * ) ( le_pri_head + le_index ) )[0] ) {
@@ -94,7 +94,7 @@
                         le_index += LE_ARRAY_DATA;
 
                         /* update primitive stack */
-                        le_sec_type --;
+                        le_sec_type -= LE_ARRAY_DATA;
 
                     }
 
@@ -102,22 +102,22 @@
                     le_flag = le_galf;
 
                     /* update head */
-                    le_sec_head += le_pri_type * LE_ARRAY_DATA;
+                    le_sec_head += le_pri_type;
 
                 } else {
 
                     /* update head */
-                    le_sec_head += le_sec_type * LE_ARRAY_DATA;
+                    le_sec_head += le_sec_type;
 
                 }
 
             }
 
             /* check detection flag */
-            if ( le_flag == 0xff ) {
+            if ( le_flag ) {
 
                 /* modulate primitive color */
-                while ( le_pri_type > 0 ) {
+                while ( le_pri_type ) {
 
                     /* update head - to data segment */
                     le_pri_head += LE_ARRAY_DATA_POSE;
@@ -131,245 +131,14 @@
                     le_pri_head ++;
 
                     /* update primitive stack */
-                    le_pri_type --;
+                    le_pri_type -= LE_ARRAY_DATA;
 
                 }
 
             } else {
 
                 /* update head */
-                le_pri_head += le_pri_type * LE_ARRAY_DATA;
-
-            }
-
-        }
-
-    }
-
-    void le_operator_get_ssd_remove_2( le_array_t * const le_array, le_array_t * const le_compare ) {
-
-        /* array sizes */
-        le_size_t le_pri_size = le_array_get_size( le_pri );
-        le_size_t le_sec_size = le_array_get_size( le_sec );
-
-        /* array base pointer */
-        le_byte_t * le_pri_base = le_array_get_byte( le_pri );
-        le_byte_t * le_sec_base = le_array_get_byte( le_sec );
-
-        /* array head pointer */
-        le_byte_t * le_pri_head = le_pri_base;
-        le_byte_t * le_pri_diff = NULL;
-        le_byte_t * le_sec_head = le_sec_base;
-
-        /* primitive type */
-        le_byte_t le_pri_type = 0;
-        le_byte_t le_sec_type = 0;
-
-        /* detection flag */
-        le_byte_t le_flag = 0x00;
-
-        /* parsing primary */
-        while ( ( le_pri_head - le_pri_base ) < le_pri_size ) {
-
-            /* extract primitive type */
-            le_pri_type = * ( le_pri_head + LE_ARRAY_DATA_POSE );
-
-            /* reset detection flag */
-            le_flag = 0x00;
-
-            /* reset secondary head */
-            le_sec_head = le_sec_base;
-
-            /* parsing secondary */
-            while ( ( le_flag == 0x00 ) && ( ( le_sec_head - le_sec_base ) < le_sec_size ) ) {
-
-                /* extract primitive type */
-                le_sec_type = * ( le_sec_head + LE_ARRAY_DATA_POSE );
-
-                /* check type compatibility */
-                if ( le_pri_type == le_sec_type ) {
-
-                    /* similarity check */
-                    if ( memcmp( le_pri_head, le_sec_head, le_pri_type * LE_ARRAY_DATA ) == 0 ) {
-
-                        /* update detection flag */
-                        le_flag = 0xff;
-
-                    }
-
-                }
-
-                /* update head */
-                le_sec_head += le_sec_type * LE_ARRAY_DATA;
-
-            }
-
-            /* analyse flag */
-            if ( le_flag == 0xff ) {
-
-                /* initialise color pointer */
-                le_pri_diff = le_pri_head + LE_ARRAY_DATA_POSE;
-
-                /* extinguish primitive color */
-                while ( le_pri_type != 0 ) {
-
-                    /* assign color components */
-                    * ( ++ le_pri_diff ) = 32;
-                    * ( ++ le_pri_diff ) = 32;
-                    * ( ++ le_pri_diff ) = 32;
-
-                    /* update color pointer */
-                    le_pri_diff = le_pri_diff + LE_ARRAY_DATA - 3;
-
-                    /* update primitive stack */
-                    le_pri_type --;
-
-                }
-
-            }
-
-            /* update head */
-            le_pri_head += le_pri_type * LE_ARRAY_DATA;
-
-        }
-
-    }
-
-    void le_operator_get_ssd_remove_1( le_array_t * const le_array, le_array_t * const le_compare ) {
-
-        /* array size */
-        le_size_t le_asize = le_array_get_size( le_array );
-        le_size_t le_csize = le_array_get_size( le_compare );
-
-        /* primitive type */
-        le_size_t le_atype = 0;
-        le_size_t le_ctype = 0;
-
-        /* array pointer */
-        le_byte_t * le_ahead = NULL;
-        le_byte_t * le_chead = NULL;
-
-        /* search index */
-        le_size_t le_parse = 0;
-        le_size_t le_index = 0;
-
-        /* detection flag */
-        le_byte_t le_flag = 0x00;
-
-        /* parsing primary */
-        while ( le_parse < le_asize ) {
-
-            /* compute array pointer */
-            le_ahead = le_array_get_byte( le_array ) + le_parse;
-
-            /* extract primitive type */
-            le_atype = * ( le_ahead + LE_ARRAY_DATA_POSE );
-
-            /* reset detection flag */
-            le_flag = 0x00;
-
-            /* reset search index */
-            le_index = 0;
-
-            /* parsing secondary */
-            while ( ( le_flag == 0x00 ) && ( le_index < le_csize ) ) {
-
-                /* compute array pointer */
-                le_chead = le_array_get_byte( le_compare ) + le_index;
-
-                /* extract primitive type */
-                le_ctype = * ( le_chead + LE_ARRAY_DATA_POSE );
-
-                /* detect matching type */
-                if ( le_atype == le_ctype ) {
-
-                    /* detection test */
-                    if ( memcmp( le_ahead, le_chead, LE_ARRAY_DATA_POSE ) == 0 ) {
-
-                        /* update detection flag */
-                        le_flag = 0xff;
-
-                    /* update search index */
-                    } else { le_index += LE_ARRAY_DATA; }
-
-                /* update search index */
-                } else { le_index += ( le_ctype + LE_ARRAY_DATA ); }
-
-            }
-
-            /* detection result analysis */
-            if ( le_flag == 0xff ) {
-
-                /* compute array pointer */
-                le_ahead += LE_ARRAY_DATA_POSE;
-
-                /* primary color modulation */
-                le_ahead[1] = 32;
-                le_ahead[2] = 32;
-                le_ahead[3] = 32;
-
-            }
-
-            /* update index */
-            le_parse += ( le_atype * LE_ARRAY_DATA );
-
-        }
-
-    }
-
-    void le_operator_get_diff_strict( le_array_t * const le_array, le_array_t * const le_compare ) {
-
-        /* array size */
-        le_size_t le_asize = le_array_get_size( le_array );
-
-        /* array size */
-        le_size_t le_csize = le_array_get_size( le_compare );
-
-        /* array pointer */
-        le_byte_t * le_head = NULL;
-
-        /* search index */
-        le_size_t le_index = 0;
-
-        /* detection flag */
-        le_byte_t le_flag = 0x00;
-
-        /* parsing primary */
-        for ( le_size_t le_parse = 0; le_parse < le_asize; le_parse += LE_ARRAY_DATA ) {
-
-            /* compute array pointer */
-            le_head = le_array_get_byte( le_array ) + le_parse;
-
-            /* reset detection flag */
-            le_flag = 0x00;
-
-            /* reset search index */
-            le_index = 0;
-
-            /* parsing secondary */
-            while ( ( le_flag == 0x00 ) && ( le_index < le_csize ) ) {
-
-                /* detection test */
-                if ( memcmp( le_head, le_array_get_byte( le_compare ) + le_index, LE_ARRAY_DATA_POSE ) == 0 ) {
-
-                    /* update detection flag */
-                    le_flag = 0xff;
-
-                /* update parser */
-                } else { le_index += LE_ARRAY_DATA; }
-
-            }
-
-            /* detection result analysis */
-            if ( le_flag == 0xff ) {
-
-                /* compute array pointer */
-                le_head += LE_ARRAY_DATA_POSE;
-
-                /* primary color modulation */
-                le_head[1] = 32;
-                le_head[2] = 32;
-                le_head[3] = 32;
+                le_pri_head += LE_ARRAY_DATA;
 
             }
 
